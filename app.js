846756f3c8a79c94438fbee5a70a2198b1d088de
M Widmaer <mwidmaer2cdmionline.com>
2018-05-15 22:36:34 +0100

    g

--- app.js
+++ app.js
@@ -13,31 +13,31 @@ var port          = process.env.PORT || 2300;
 global.hostname   = process.env.HOSTNAME || "18.221.93.144";
 hostname = hostname.toLowerCase();
 global.hostport   = port;
-global.hosturl    = "http://"+global.hostname+":"+hostport;
+global.hosturl    = "https://"+hostname+":"+hostport;
 console.log("URL: ", hosturl);
 
 var http          = require('http').Server(app);
 
-// if(is_ssl) {
-//     if(hostname == "test.zeedas.com"){
-//         console.log("hostname tested = ", hostname);
-//         var privateKey = fs.readFileSync('/etc/letsencrypt/live/test.zeedas.com-0001/privkey.pem', 'utf8');
-//         var certificate = fs.readFileSync('/etc/letsencrypt/live/test.zeedas.com-0001/cert.pem', 'utf8');
-//     }else if(hostname == "demo.zeedas.com"){
-//         console.log("hostname demo = ", hostname);
-//         var privateKey = fs.readFileSync('/etc/letsencrypt/live/demo.zeedas.com/privkey.pem', 'utf8');
-//         var certificate = fs.readFileSync('/etc/letsencrypt/live/demo.zeedas.com/cert.pem', 'utf8');
-//     }else{
-//         console.log("hostname others = ", hostname);
-//         var privateKey = fs.readFileSync('/etc/letsencrypt/live/zeedas.com-0001/privkey.pem', 'utf8');
-//         var certificate = fs.readFileSync('/etc/letsencrypt/live/zeedas.com-0001/cert.pem', 'utf8');
-//     }
-//     var credentials = {key: privateKey, cert: certificate};
-//     var https       = require('https').Server(credentials, app);
-//     var io          = require('socket.io')(https)
-// }else{
-//     var io = require('socket.io')(http);
-// }
+if(!is_ssl) {
+    if(hostname == "test.zeedas.com"){
+        console.log("hostname tested = ", hostname);
+        var privateKey = fs.readFileSync('/etc/letsencrypt/live/test.zeedas.com-0001/privkey.pem', 'utf8');
+        var certificate = fs.readFileSync('/etc/letsencrypt/live/test.zeedas.com-0001/cert.pem', 'utf8');
+    }else if(hostname == "demo.zeedas.com"){
+        console.log("hostname demo = ", hostname);
+        var privateKey = fs.readFileSync('/etc/letsencrypt/live/demo.zeedas.com/privkey.pem', 'utf8');
+        var certificate = fs.readFileSync('/etc/letsencrypt/live/demo.zeedas.com/cert.pem', 'utf8');
+    }else{
+        console.log("hostname others = ", hostname);
+        var privateKey = fs.readFileSync('/etc/letsencrypt/live/zeedas.com-0001/privkey.pem', 'utf8');
+        var certificate = fs.readFileSync('/etc/letsencrypt/live/zeedas.com-0001/cert.pem', 'utf8');
+    }
+    var credentials = {key: privateKey, cert: certificate};
+    var https       = require('https').Server(credentials, app);
+    var io          = require('socket.io')(https)
+}else{
+    var io = require('socket.io')(http);
+}
 var io = require('socket.io')(http);
 //  DB Connection =============================================================
 var connection = mongoose.connect(configDB.staging, {useMongoClient: true}, function(err) {
@@ -101,10 +101,10 @@ require('./modules/subs/subscriptions');
 
 // require('./database/seeders/index.js');
 // launch ======================================================================
-// if(is_ssl) {
-//     https.listen(port);
-// }else{
+if(!is_ssl) {
+    https.listen(port);
+}else{
     http.listen(port);
-// }
+}
 
-console.log('listening on port:' + port);
+console.log('listening on localhost:' + port+ (is_ssl?" (SSL)":""));
