e1078399a51c5d507ac43a4c9e79c02502c730cb
ahmanemmanuel@gmail.com <ahmanemmanuel@gmail.com>
2018-08-22 13:55:34 +0100

    deploying test

--- routes/Users.js
+++ routes/Users.js
@@ -10,5 +10,8 @@ module.exports = function (app) {
 
 	// shouldnt be here
 	app.post('/Invite', UserMgt.invite_user);
-	app.get('/get_clients', isLoggedIn,UserMgt.get_clients)
+	app.get('/get_clients', isLoggedIn,UserMgt.get_clients);
+	app.get('/s', (req, res)=>{
+		res.send('Invites');
+	})
 }
